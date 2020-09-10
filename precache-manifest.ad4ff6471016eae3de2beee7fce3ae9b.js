120cbc4a3154419f134215efc7a2ef46615c0dbb
Mikel Münnekhoff <mikelmunnekhoff@Mikels-MacBook-Pro.local>
2020-09-10 12:42:10 +0200

    Updates

---
+++ precache-manifest.ad4ff6471016eae3de2beee7fce3ae9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94e782d923400aff37b2559bfd3c940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d18036c34e78a0378c",
+    "url": "/static/css/main.ba8c6910.chunk.css"
+  },
+  {
+    "revision": "787b867e3247c6fa08aa",
+    "url": "/static/js/2.fc6986fa.chunk.js"
+  },
+  {
+    "revision": "ee8909476e1daa379d46b6d7286404f2",
+    "url": "/static/js/2.fc6986fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d18036c34e78a0378c",
+    "url": "/static/js/main.af712a23.chunk.js"
+  },
+  {
+    "revision": "b756801a6c9e86fd5861",
+    "url": "/static/js/runtime-main.76600a31.js"
+  }
+]);
